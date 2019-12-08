9a71b9ec768e835c3a5215ce6b59f474007c4ffd
qiuzhiqian <xia_mengliang@163.com>
2019-12-08 22:12:38 +0800

    添加前端登录页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,128 +1,21 @@
 <template>
-    <div class="layout">
-        <Layout style="height:100%;">
-            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-                <Menu :active-name="activeName" theme="dark" width="auto" :class="menuitemClasses" @on-select="itemChange">
-                    <MenuItem name="1-1">
-                        <Icon type="ios-cube" />
-                        <span>Devices</span>
-                    </MenuItem>
-                    <MenuItem name="1-2">
-                        <Icon type="ios-pulse" />
-                        <span>Monitor</span>
-                    </MenuItem>
-                    <MenuItem name="1-3">
-                        <Icon type="ios-navigate" />
-                        <span>Map</span>
-                    </MenuItem>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header :style="{padding: 0}" class="layout-header-bar">
-                    <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 10px'}" type="md-menu" size="24"></Icon>
-                </Header>
-                <Content :style="{margin: '10px', background: '#fff', minHeight: '260px'}">
-                    <Devices v-if="activeName == '1-1'"></Devices>
-                    <GpsTable v-else-if="activeName =='1-2'"></GpsTable>
-                    <MapWidget v-else></MapWidget>
-                </Content>
-                <Footer class="layout-footer-center">2019 &copy; xiamengliang</Footer>
-            </Layout>
-        </Layout>
-    </div>
+    <MainPage v-if="pageSelect == 'main'"></MainPage>
+    <Login v-else></Login>
 </template>
 <script>
-import MapWidget from './components/MapWidget'
-import Devices from './components/Devices'
-import GpsTable from './components/GpsTable'
+import MainPage from './components/MainPage'
+import Login from './components/Login'
     export default {
-        data () {
-            return {
-                isCollapsed: false,
-                activeName: '1-1'
-            }
-        },
-        computed: {
-            rotateIcon () {
-                return [
-                    'menu-icon',
-                    this.isCollapsed ? 'rotate-icon' : ''
-                ];
-            },
-            menuitemClasses () {
-                return [
-                    'menu-item',
-                    this.isCollapsed ? 'collapsed-menu' : ''
-                ]
-            }
-        },
-        methods: {
-            collapsedSider () {
-                this.$refs.side1.toggleCollapse();
-            },
-            itemChange(str){
-                this.activeName = str
+        data(){
+            return{
+                pageSelect:'login'
             }
         },
         components:{
-          MapWidget,
-          Devices,
-          GpsTable
+          MainPage,
+          Login
         }
     }
 </script>
 <style scoped>
-    .layout{
-        border: 1px solid #d7dde4;
-        background: #f5f7f9;
-        position: relative;
-        border-radius: 4px;
-        overflow: hidden;
-        height: 800px;
-    }
-    .layout-header-bar{
-        background: #fff;
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
-    }
-    .layout-logo-left{
-        width: 90%;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;
-    }
-    .menu-icon{
-        transition: all .3s;
-    }
-    .rotate-icon{
-        transform: rotate(-90deg);
-    }
-    .menu-item span{
-        display: inline-block;
-        overflow: hidden;
-        width: 69px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        vertical-align: bottom;
-        transition: width .2s ease .2s;
-    }
-    .menu-item i{
-        transform: translateX(0px);
-        transition: font-size .2s ease, transform .2s ease;
-        vertical-align: middle;
-        font-size: 16px;
-    }
-    .collapsed-menu span{
-        width: 0px;
-        transition: width .2s ease;
-    }
-    .collapsed-menu i{
-        transform: translateX(5px);
-        transition: font-size .2s ease .2s, transform .2s ease .2s;
-        vertical-align: middle;
-        font-size: 22px;
-    }
-    .layout-footer-center{
-        text-align: center;
-    }
 </style>
